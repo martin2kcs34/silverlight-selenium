05fbf3ee09012a213357c9c789b0a7a2b959ab33
sachin.sudheendra <sachin.sudheendra@83ffe396-c6ca-11dd-a8b6-f5aca7b582bb>
2009-01-08 04:41:44 +0000

    .NET client for Silvernium <Sachin Sudheendra>

---
+++ dotnet/Silvernium/IntegrationTests/SilverniumIntegrationTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using Selenium;
+using ThoughtWorks.Selenium.Silvernium;
+
+namespace IntegrationTests
+{
+    [TestFixture]
+    public class SilverniumIntegrationTests
+    {
+
+        private ISelenium selenium;
+        private Silvernium silvernium;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            selenium = new DefaultSelenium("localhost", 4444, "*iexplore", "http://localhost");
+            selenium.Start();
+            selenium.Open("http://localhost");
+            silvernium = new Silvernium(selenium, "Test");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            selenium.Stop();
+        }
+
+        [Test]
+        public void ShouldSpawnBrowserOnSeleniumAndFetchSilverLightJSStringPreixForMSIE()
+        {
+            Assert.AreEqual("window.document['Test'].",silvernium.SilverLightJSStringPrefix);
+        }
+    }
+}
