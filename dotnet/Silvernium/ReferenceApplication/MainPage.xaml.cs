ae81896ed9d3833d519465e1f994a48a4731fb6d
marcelo.farias <marcelo.farias@gmail.com>
2011-08-24 22:39:44 +0000

    Added reference ComboBoxes

--- dotnet/Silvernium/ReferenceApplication/MainPage.xaml.cs
+++ dotnet/Silvernium/ReferenceApplication/MainPage.xaml.cs
@@ -1,12 +1,28 @@
-﻿using System.Windows.Controls;
+﻿using System.Collections.ObjectModel;
+using System.Windows.Controls;
 
 namespace DBServer.Selenium.Silvernium.ReferenceApplication
 {
-    public partial class MainPage : UserControl
+    public partial class MainPage
     {
         public MainPage()
         {
             InitializeComponent();
+
+            displayMemberPathComboBox.ItemsSource = new Collection<Person>
+            {
+                new Person {Id = 1, Name = "Arthur"},
+                new Person {Id = 2, Name = "John"},
+                new Person {Id = 3, Name = "Richard"}
+            };
+            displayMemberPathComboBox.SelectedIndex = 0;
+
         }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
